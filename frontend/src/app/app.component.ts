2ca4793cd74d78a64e8c3422a287359601ec9bae
NikolaV000 <nikolavasiljevic650@gmail.com>
2025-04-22 17:03:56 +0200

    feat: Created column layaut and add Bootstrap

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BacklogComponent } from "./shared/components/backlog/backlog.component";
+import { TodoComponent } from './shared/components/todo/todo.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    BacklogComponent,
+    TodoComponent
+  ]
 })
-export class AppComponent {
-  title = 'List';
-  to_do = 'To Do';
+export class AppComponent {;
 }
---
+++ frontend/src/app/shared/components/backlog/backlog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-backlog',
+  imports: [],
+  templateUrl: './backlog.component.html',
+  styleUrl: './backlog.component.css',
+  standalone: true
+})
+export class BacklogComponent {
+
+}
