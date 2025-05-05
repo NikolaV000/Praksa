d9b79bbe451e62a2001bc1e9e4f6b01ddcf73787
NikolaV000 <nikolavasiljevic650@gmail.com>
2025-05-05 21:29:06 +0200

    feat: Edit dialog

--- frontend/src/app/shared/components/todo/todo.component.ts
+++ frontend/src/app/shared/components/todo/todo.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [CdkDropList, CdkDrag],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css',
   standalone: true
 })
 export class TodoComponent {
+  todo = ["Task1"];
+
+  done = ["Task1"];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+
 
 }
